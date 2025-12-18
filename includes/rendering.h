dab6ac214d51d58ad857675c22816ad007c8c28b
NekoTintin <tintin361official@gmail.com>
2025-12-18 11:30:11 +0100

    Added struct for t_ray

--- includes/rendering.h
+++ includes/rendering.h
@@ -3,9 +3,29 @@
 
 # include "cub3D.h"
 
+typedef struct s_ray
+{
+	double	camera_x;
+	double	dir_x;
+	double	dir_y;
+	int		map_x;
+	int		map_y;
+	int		step_x;
+	int		step_y;
+	double	sidedist_x;
+	double	sidedist_y;
+	double	deltadist_x;
+	double	deltadist_y;
+	double	wall_dist;
+	double	wall_x;
+	int		side;
+	int		line_height;
+	int		draw_start;
+	int		draw_end;
+}				t_ray;
+
 void		render(t_game *game);
 void		init_ray(t_ray *ray);
 int			raycasting(t_player *player, t_game *game);
-
 
 #endif
